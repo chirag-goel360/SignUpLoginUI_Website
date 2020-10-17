1ac00b4297ccf9bb6b6aa37bbd6449a31ccf1ddb
chirag-goel360 <lastknight03111998@gmail.com>
2020-10-17 14:33:58 +0530

    Model of User

--- morsecodegeneration/models.py
+++ morsecodegeneration/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    fname = models.CharField(max_length=40,null=False)
+    email = models.EmailField(max_length=30,default='',null=False)
+    phoneno = models.CharField(max_length=10,default='', null=False)
+    password = models.CharField(max_length=15, default='', null=False)
